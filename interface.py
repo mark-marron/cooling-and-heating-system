69bc890c3a7fe6fc7f03c510de649a9176953eb4
SeanMal1 <120354053@umail.ucc.ie>
2022-11-11 16:25:47 +0000

    Updated interface.py functionality
    Now displays 'Set Temperature' value and the difference between the current temperature and this value. The current temperature is now only displayed once the 'Get Temperature' button has been pressed. The 'Toggle Heating' and 'Toggle Cooling' displays are now updated to show the current state that the heating/cooling is in respectively. A small piece of text has been added which is displayed when the user presses 'How to use'. This informs the user on how to use the application.

--- interface.py
+++ interface.py
@@ -11,6 +11,9 @@ zonePanels = []
 temp = tempSensor
 currentTempint = z1.get_temp()
 currentTemp = str(currentTempint)
+heat_on = True
+cool_on = True
+tutorial_on = True
 
 root = tk.Tk()
 def getCurrentTemp():
@@ -19,8 +22,6 @@ def getCurrentTemp():
     return currentTemp
 
     
-
-
 root.title('Heating and Cooling System Controller')
 root.rowconfigure(0, minsize=200, weight=1)
 root.columnconfigure(1, minsize=200, weight=1)
@@ -30,34 +31,91 @@ frameButtons = tk.Frame(root, relief=tk.RAISED, bd=2)
 
 label = tk.Label(text="Heating and Cooling System Controller", fg="black")
 
-setTemp = tk.Button(frameButtons,text="Set Temperature")
-setTemp.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+def setTempClicked():
+    try:
+        t = int(setTempValueInt.get())
+        tempDiff = t - int(currentTemp)
+        result = "You have set the temperature to %i Degrees Celsius!\n There is a %i Degree difference from the current temperature" %(t, tempDiff)
+        selectedTemp.config(text=result)
+    except ValueError as error:
+        selectedTemp.config(title='Error', message=error)
 
-getTemp = tk.Button(frameButtons,text="Get Temperature", command=getCurrentTemp)
-getTemp.grid(row=1, column=0, sticky="ew", padx=5)
+def getTempClicked():
+    curTemp = currentTemp
+    result2 = "Current Temperature is : %s Degrees Celsius" %(curTemp)
+    getTempValue.config(text=result2)
+
+def toggleHeatClick():
+    global heat_on
+    if heat_on:
+        result3 = "Heating : ON"
+        heatingToggle.config(text=result3)
+        heat_on = False
+    else:
+        result3 = "Heating : OFF"
+        heatingToggle.config(text=result3)
+        heat_on = True
+
+def toggleCoolClick():
+    global cool_on
+    if cool_on:
+        result4 = "Cooling : ON"
+        coolingToggle.config(text=result4)
+        cool_on = False
+    else:
+        result4 = "Cooling : OFF"
+        coolingToggle.config(text=result4)
+        cool_on = True
+
+def tutorialClicked():
+    global tutorial_on
+    if tutorial_on:
+        result5 = "By clicking 'Get Temperature' the current temperature reading is displayed.\nBy enterning a value into the box next to 'Set Temperature' and then clicking the button this allows you to select a specific temperature you would like the room to reach\nBy clicking 'Toggle Heating' the Biomass Heating is turned on\nBy clicking 'Toggle Cooling' the Cooling Fans are turned on\n You can also close the Tutorial text by clicking 'How to use' once again"
+        tutorialText.config(text=result5)
+        tutorial_on = False
+    else:
+        result5 = ""
+        tutorialText.config(text=result5)
+        tutorial_on = True
+
+getTemp = tk.Button(frameButtons,text="Get Temperature")
+getTemp.grid(row=0, column=0, sticky="ew", padx=5)
+getTemp.configure(command=getTempClicked)
+
+setTemp = tk.Button(frameButtons,text="Set Temperature")
+setTemp.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+setTemp.configure(command=setTempClicked)
 
 toggleHeating = tk.Button(frameButtons,text="Toggle Heating")
 toggleHeating.grid(row=2, column=0, sticky="ew", padx=5)
+toggleHeating.configure(command=toggleHeatClick)
 
 toggleCooling = tk.Button(frameButtons,text="Toggle Cooling")
 toggleCooling.grid(row=3, column=0, sticky="ew", padx=5)
+toggleCooling.configure(command=toggleCoolClick)
 
 tutorial = tk.Button(frameButtons,text="How to use")
 tutorial.grid(row=4, column=0, sticky="ew", padx=5)
+tutorial.configure(command=tutorialClicked)
 
-setTempValue = Entry(answerWindow, bg="light yellow")
-setTempValue.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+setTempValueInt = IntVar()
+setTempValue = Entry(answerWindow, bg="light yellow", textvariable=setTempValueInt)
+setTempValue.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
 
-getTempValue = tk.Label(answerWindow, text="Current Temp: " + currentTemp)
-getTempValue.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+getTempValue = tk.Label(answerWindow, text="Current Temperature is :")
+getTempValue.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 
-heatingToggle = tk.Label(answerWindow, text="{Heating : on/off}")
+selectedTemp = tk.Label(answerWindow)
+selectedTemp.grid(row=5, column=0, sticky="ew", padx=5,pady=5)
+
+heatingValue = StringVar()
+heatingToggle = tk.Label(answerWindow, text="Heating default state is OFF")
 heatingToggle.grid(row=2, column=0, sticky="ew", padx=5)
 
-coolingToggle = tk.Label(answerWindow, text="{Cooling : on/off}")
+coolingToggle = tk.Label(answerWindow, text="Cooling default state is OFF")
 coolingToggle.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
 
-tutorialText = tk.Label(answerWindow, text="{how to use the controller}")
+tutorialText = tk.Label(answerWindow, text="")
 tutorialText.grid(row=4, column=0, sticky="ew", padx=5)
 
 frameButtons.grid(row=0, column=0, sticky="ns")
