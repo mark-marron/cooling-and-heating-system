2eb8fbfc42260b76a6c92415a6cd41569f3536f9
SeanMal1 <120354053@umail.ucc.ie>
2022-11-17 22:09:20 +0000

    Update TempSensor.py
    temp sensor now uses an internet crawler to take the current temperature in cork rather than a random value

--- TempSensor.py
+++ TempSensor.py
@@ -1,5 +1,6 @@
 from random import randint
-
+from bs4 import BeautifulSoup
+import requests
 
 class TempSensor:
 
@@ -7,4 +8,14 @@ class TempSensor:
         self._temp = randint(18, 25)
 
     def get_temp(self):
+        headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+                }
+        req = requests.get(
+           f'https://www.google.com/search?q=+cork+weather+&oq=+cork+weather+&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+        
+        soup = BeautifulSoup(req.text ,'html.parser')
+        temperature = soup.select('#wob_tm')[0].getText().strip()
+        self._temp = temperature
+      
         return self._temp
