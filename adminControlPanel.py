83d14623bc696a2c9666f305c17687262044b614
SeanMal1 <120354053@umail.ucc.ie>
2022-11-17 22:13:16 +0000

    Update adminControlPanel.py
    now cooling and heating system states are set when in toggle_fan, toggle_heater and in the __init__
    
    The state is set to 5 in __init__ because as discussed in section 4.2 of Team21 deliverable the initial/default state is off

--- adminControlPanel.py
+++ adminControlPanel.py
@@ -7,6 +7,7 @@ class AdminControl:
         self._target_temp = 22
         self._avg_temp = 22
         self._time_remaining = 180  # Time in Seconds
+        self._state = 5
 
     def get_avg_temp(self):
         return self._avg_temp
@@ -25,12 +26,16 @@ class AdminControl:
         
 
     def set_timer(self, input_time):
+        if input_time is not int:
+            raise TypeError("An integer number is supposed to be entered.")
+        if input_time < 0:
+            raise ValueError("A positive integer number is supposed to be entered")
         self._total_seconds = input_time
         return "time set"
         
 
     def toggle_fan(self):
-        pass
+        self._state = 1
 
     def toggle_heater(self):
-        pass
+        self._state = 4
