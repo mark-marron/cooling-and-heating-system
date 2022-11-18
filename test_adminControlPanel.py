a1a8f2feff3633d49ee99aaf2212854c4d4a1c6d
SeanMal1 <120354053@umail.ucc.ie>
2022-11-18 12:50:37 +0000

    Create test_adminControlPanel.py
    The unit testing for adminControlPanel.py - in order to test use the command "python -m unittest test_adminControlPanel.py" in the terminal

---
+++ test_adminControlPanel.py
@@ -0,0 +1,42 @@
+import unittest
+from adminControlPanel import AdminControl
+
+a1 = AdminControl
+class Testset_target_temp(unittest.TestCase):
+    def test_temp_type(self):
+        #Test temp when temp when values are of different type
+        self.assertRaises(TypeError, a1.set_target_temp, a1, 'q')
+
+        self.assertRaises(TypeError, a1.set_target_temp, a1, 17.929)
+
+        self.assertRaises(TypeError, a1.set_target_temp, a1, True)
+
+        self.assertRaises(TypeError, a1.set_target_temp, a1, [8,9,0,3,5])
+
+        self.assertRaises(TypeError, a1.set_target_temp, a1, {'key':'item'})
+
+    def test_temp_values(self):
+        #Test temp when temp values are of correct type but outside the range specified in interface.py
+        a1.set_target_temp(a1,-6)
+        self.assertAlmostEqual(a1.get_target_temp(a1), 0)
+
+        a1.set_target_temp(a1,500)
+        self.assertAlmostEqual(a1.get_target_temp(a1), 30)
+
+        a1.set_target_temp(a1,4)
+        self.assertAlmostEqual(a1.get_target_temp(a1), 4)
+
+class Testset_timer(unittest.TestCase):
+    def test_time_type(self):
+        #Test time when values are of different type
+        self.assertRaises(TypeError, a1.set_timer, a1, 'ewfewh')
+
+        self.assertRaises(TypeError, a1.set_timer, a1, 2.989474)
+
+        self.assertRaises(TypeError, a1.set_timer, a1, True)
+
+        self.assertRaises(TypeError, a1.set_timer, a1, [6,7,8,9])
+
+        self.assertRaises(TypeError, a1.set_timer, a1, {'key':'item'})
+
+        self.assertRaises(ValueError, a1.set_timer, a1, -5)
