2b9ab865767e4b7ab72f8fdff90a54f9f25921ad
SeanMal1 <120354053@umail.ucc.ie>
2022-11-18 12:48:43 +0000

    Update adminControlPanel.py
    includes new TypeErrors and ValuesErrors raised to go with the unit testing for adminControlPanel

--- adminControlPanel.py
+++ adminControlPanel.py
@@ -4,7 +4,7 @@ import datetime
 class AdminControl:
 
     def __init__(self):
-        self._target_temp = 22
+        self._target_temp = 0
         self._avg_temp = 22
         self._time_remaining = 180  # Time in Seconds
         self._state = 5
@@ -13,7 +13,26 @@ class AdminControl:
         return self._avg_temp
 
     def set_target_temp(self, input_target):
-        self._target_temp = input_target
+        if type(input_target) == str:
+            raise TypeError("Please enter a whole positive integer, not a string.")
+        if type(input_target) == bool:
+            raise TypeError("Please enter a whole positive integer, not a boolean.")
+        if type(input_target) == list:
+            raise TypeError("A positive integer number is supposed to be entered, not a list")
+        if type(input_target) == dict:
+            raise TypeError("A positive integer number is supposed to be entered, not a dictionary")
+        if type(input_target) == int:
+            if input_target > 0 and input_target < 30:
+                self._target_temp = input_target
+            if input_target >= 30:
+                self._target_temp = 30
+            if input_target <= 0:
+                self._target_temp = 0
+        if type(input_target) == float:
+            raise TypeError("Please enter a whole positive integer, not a float.")
+
+    def get_target_temp(self):
+        return self._target_temp
 
     def get_timer(self):
         self._timer_end_msg = "Timer finished!"
@@ -26,12 +45,22 @@ class AdminControl:
         
 
     def set_timer(self, input_time):
-        if input_time is not int:
-            raise TypeError("An integer number is supposed to be entered.")
-        if input_time < 0:
-            raise ValueError("A positive integer number is supposed to be entered")
-        self._total_seconds = input_time
-        return "time set"
+        if type(input_time) == str:
+            raise TypeError("A positive integer number is supposed to be entered, not a string")
+        if type(input_time) == float:
+            raise TypeError("A positive integer number is supposed to be entered, not a float")
+        if type(input_time) == bool:
+            raise TypeError("A positive integer number is supposed to be entered, not a boolean")
+        if type(input_time) == list:
+            raise TypeError("A positive integer number is supposed to be entered, not a list")
+        if type(input_time) == dict:
+            raise TypeError("A positive integer number is supposed to be entered, not a dictionary")
+        if type(input_time) == int:
+            if input_time < 0:
+                raise ValueError("A positive integer number is supposed to be entered, not a negative value")
+            if input_time > 0:
+                self._total_seconds = input_time
+                return "time set"
         
 
     def toggle_fan(self):
