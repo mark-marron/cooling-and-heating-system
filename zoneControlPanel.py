cf07efb04a8e5d414a2522e0464c11e7edf07cc5
SeanMal1 <120354053@umail.ucc.ie>
2022-11-18 16:54:41 +0000

    Update zoneControlPanel.py
    updated the zone control panel to be similar to the admin control panel without the option to set or get rooms. As shown in figure 4.1.1 in the Team21 deliverable the zone control panel configures its settings through the admin control panel hence the methods in this class use the admin control methods

--- zoneControlPanel.py
+++ zoneControlPanel.py
@@ -1,12 +1,34 @@
 from TempSensor import TempSensor
-
+from adminControlPanel import AdminControl
 
 class ZoneControl:
     def __init__(self):
         self._temp_sensor = TempSensor()
-        self._temp = self._temp_sensor.get_temp()
+        self._target_temp = 0
+        self._total_seconds = 0
+        self._state = 5
+        self._temp = self._temp_sensor.get_outside_temp()
         self._heater_running = False
         self._fans_running = False
 
     def get_temp(self):
-        return self._temp_sensor.get_temp()
+        return self._temp_sensor.get_outside_temp()
+
+    def set_target_temp(self, input_target):
+        AdminControl.set_target_temp(AdminControl, input_target)
+
+    def get_target_temp(self):
+        self._target_temp = AdminControl.get_target_temp(AdminControl)
+        return self._target_temp
+
+    def set_timer(self, input_time):
+        self._total_seconds = AdminControl.set_timer(AdminControl,input_time)
+        
+    def get_timer(self):
+        return AdminControl.get_timer(AdminControl)
+
+    def toggle_fan(self):
+        self._state = 1
+
+    def toggle_heater(self):
+        self._state = 4
