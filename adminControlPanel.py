0522e188279c55391d0d4a28544ec8b7feca1f43
Joseph O'Donovan <118483276@umail.ucc.ie>
2022-11-18 13:08:49 +0000

    Changed avg_temp, added method for calculating avg temp
    List of temperatures taken in from TempSensor written to list and average of list returned.
    Working on pulling temps from list on a per time basis

--- adminControlPanel.py
+++ adminControlPanel.py
@@ -1,16 +1,31 @@
 import time
 import datetime
 
+
 class AdminControl:
 
     def __init__(self):
+        self._timer = None
+        self._total_seconds = None
+        self._timer_end_msg = None
         self._target_temp = 22
-        self._avg_temp = 22
         self._time_remaining = 180  # Time in Seconds
         self._state = 5
 
+    def avg_temp(self, temp):
+        avg_temp_list = []
+        if avg_temp_list is not None:
+            if len(avg_temp_list) >= 10:
+                avg_temp_list.pop(0)
+                avg_temp_list.append(temp)
+            else:
+                avg_temp_list.append(temp)
+        return sum(avg_temp_list) / len(avg_temp_list)
+
+
+
     def get_avg_temp(self):
-        return self._avg_temp
+        return self
 
     def set_target_temp(self, input_target):
         self._target_temp = input_target
@@ -18,12 +33,11 @@ class AdminControl:
     def get_timer(self):
         self._timer_end_msg = "Timer finished!"
         while self._total_seconds > 0:
-            self._timer = datetime.timedelta(seconds = self._total_seconds)
+            self._timer = datetime.timedelta(seconds=self._total_seconds)
             print(self._timer, end="\r")
             time.sleep(1)
             self._total_seconds = self._total_seconds - 1
         return self._timer_end_msg
-        
 
     def set_timer(self, input_time):
         if input_time is not int:
@@ -32,7 +46,6 @@ class AdminControl:
             raise ValueError("A positive integer number is supposed to be entered")
         self._total_seconds = input_time
         return "time set"
-        
 
     def toggle_fan(self):
         self._state = 1
