2572d8834df8bf5d3534fede4480f80cc5cef83e
Joseph O'Donovan <118483276@umail.ucc.ie>
2022-11-21 23:56:16 +0000

    Added test calls of temperature physics

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from adminControlPanel import AdminControl
 from zoneControlPanel import ZoneControl
-from TempSensor import TempSensor
+from time import sleep
 
 
 running = True
@@ -8,7 +8,13 @@ adminPanel = AdminControl()
 z1 = ZoneControl()
 z2 = ZoneControl()
 zonePanels = []
+z1.temperature_physics()  # calling temperature_physics function
+z1.set_target_temp(12)  # setting target temp to 12 deg
 
 while running:
-    print("Zone 1 Temp: ", z1.get_temp())  # Prints a string
-    print("Average Temp:", adminPanel.avg_temp())  # Prints an int
+    # print("Zone 1 Temp: ", z1.get_temp())  # Prints a string
+    # print("Average Temp:", adminPanel.avg_temp())  # Prints an int
+    print("Zone 1:")
+    print(z1.get_zone_temp())
+    print(z1.get_state())
+    sleep(5)
