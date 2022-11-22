1281bd14c51ede9b4a01b3e2cfd6286a8763ce85
Joseph <118483276@umail.ucc.ie>
2022-11-22 15:51:57 +0000

    Created _tminus_ambient_temp_decrease variable so that when State = 5 the temperature will climb to ambient Implemented into physics

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ z1 = ZoneControl()
 z2 = ZoneControl()
 zonePanels = []
 
-z1.set_target_temp(12)  # setting target temp to 12 deg
+z1.set_target_temp(0)  # setting target temp to 12 deg
 
 while running:
     # print("Zone 1 Temp: ", z1.get_temp())  # Prints a string
