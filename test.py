627b88fcb3624d4a2605fc3a8430c7735733ffb2
Joseph O'Donovan <118483276@umail.ucc.ie>
2022-11-22 01:06:20 +0000

    Added more tests and prints to see variables when testing

--- test.py
+++ test.py
@@ -8,13 +8,16 @@ adminPanel = AdminControl()
 z1 = ZoneControl()
 z2 = ZoneControl()
 zonePanels = []
-z1.temperature_physics()  # calling temperature_physics function
+
 z1.set_target_temp(12)  # setting target temp to 12 deg
 
 while running:
     # print("Zone 1 Temp: ", z1.get_temp())  # Prints a string
     # print("Average Temp:", adminPanel.avg_temp())  # Prints an int
+    z1.temperature_physics()  # calling temperature_physics function
     print("Zone 1:")
     print(z1.get_zone_temp())
+    print(z1.get_temp())
+    print(z1.get_target_temp())
     print(z1.get_state())
     sleep(5)
