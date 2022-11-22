361c08283785637770d5ede2b2b194476149c58d
Joseph O'Donovan <118483276@umail.ucc.ie>
2022-11-22 01:05:24 +0000

    Added more functionality to temperature_physics, Fixed get_state() not being called

--- zoneControlPanel.py
+++ zoneControlPanel.py
@@ -1,6 +1,7 @@
 from TempSensor import TempSensor
 from adminControlPanel import AdminControl
 from time import sleep
+from random import randint
 
 
 class ZoneControl:
@@ -14,10 +15,11 @@ class ZoneControl:
         self._total_seconds = 0
         self._state = 5
         self._temp = self._temp_sensor.get_outside_temp()
-        self._zone_temp = self._temp
+        self._zone_temp = randint(8, 15)
         self._heater_running = False
         self._fans_running = False
-        self._tminus_ambient_temp_decrease = 30  # Time is takes until _zone_temp decreases when Fan and Heating are off, ambient temperature drop via losses
+        self._tminus_ambient_temp_decrease = 30  # Time is takes until _zone_temp decreases when Fan and Heating are
+        # off, ambient temperature drop via losses
         self._tminus_temp_decrease = 5  # Time takes until _zone_temp decreases by 1 Degree
         self._tminus_temp_increase = 5   # Time takes until _zone_temp increases by 1 Degree
 
@@ -29,10 +31,6 @@ class ZoneControl:
         return self._temp_sensor.get_outside_temp()
 
     def get_zone_temp(self):
-        return self._zone_temp
-
-    def set_zone_temp(self):
-        self._zone_temp += 1
         return self._zone_temp
 
     '''
@@ -88,15 +86,19 @@ class ZoneControl:
         self._state = 5
 
     def temperature_physics(self):
-        if self.get_state == 5:  # If heating and fan are off
+        if self._target_temp > self._zone_temp:
+            self.toggle_heater()
+        elif self._target_temp < self._zone_temp:
+            self.toggle_fan()
+
+        if self.get_state() == 5:  # If heating and fan are off
             if self._zone_temp > self._temp:
                 self._temp -= 1
                 sleep(self._tminus_ambient_temp_decrease)  # 1 deg decrease in temp via ambient losses
             elif self._zone_temp <= self._temp:
                 self._zone_temp = self._temp
 
-        if self.get_state == 4:  # If heating is on
-            print("heating is on(placeholder)")  # TEST REMOVE
+        elif self.get_state() == 4:  # If heating is on
             if self._target_temp == self._zone_temp:
                 self.reset_state()  # Turn heating off
             elif self._target_temp > self._zone_temp:
@@ -106,13 +108,12 @@ class ZoneControl:
                 self.toggle_fan()
                 sleep(self._tminus_temp_decrease)  # 1 deg decrease in temp takes _tminus_temp_decrease seconds
 
-        if self.get_state == 1:  # If fan is on
-            print("fan is on(placeholder)")  # TEST REMOVE
+        elif self.get_state() == 1:  # If fan is on
             if self._target_temp == self._zone_temp:
                 self.reset_state()
             elif self._target_temp < self._zone_temp:
                 self._zone_temp -= 1
                 sleep(self._tminus_temp_decrease)  # 1 deg decrease in temp takes _tminus_temp_increase seconds
-            elif self._target_temp < self._zone_temp:
+            elif self._target_temp > self._zone_temp:
                 self.toggle_heater()
                 sleep(self._tminus_temp_decrease)  # 1 deg increase in temp takes _tminus_temp_decrease seconds
