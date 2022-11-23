46781e9c342c39767121091ebaa790290a62c39c
Joseph O'Donovan <118483276@umail.ucc.ie>
2022-11-23 20:54:39 +0000

    Created new_temperature_physics
    Created a new function to replace old temperature_physics, new function is run when called, it calculates using a timer, _start_time and _time_taken.
    _time_taken = "Time right now" - _start_time.
    
    When the function runs through it no longer waits _tminus_temp_increase or equivalent, blocking, but now calculated the time taken and if _time_taken % _tminus_temp_increase = 0 or equivalent it will add to _zone_temp _time_taken / variable _tminus_temp_increase
    
    The state checks and function is much the same as old function except this function only has to be run when an update in _zone_temp is required instead of running constantly and blocking.

--- test.py
+++ test.py
@@ -10,15 +10,16 @@ z1 = ZoneControl()
 z2 = ZoneControl()
 zonePanels = []
 
-z1.set_target_temp(0)  # setting target temp to 12 deg
+z1.set_target_temp(20)  # setting target temp to 12 deg
 
 while running:
     # print("Zone 1 Temp: ", z1.get_temp())  # Prints a string
     # print("Average Temp:", adminPanel.avg_temp())  # Prints an int
-    asyncio.run(z1.temperature_physics())  # calling temperature_physics function
+    # asyncio.run(z1.temperature_physics())  # calling temperature_physics function
+    z1.new_temperature_physics()
     print("Zone 1:")
     print("zone temp", z1.get_zone_temp())
     print("outside temp", z1.get_temp())
     print("target temp", z1.get_target_temp())
     print("zone state", z1.get_state())
-    sleep(5)
+    sleep(1)
--- zoneControlPanel.py
+++ zoneControlPanel.py
@@ -1,8 +1,8 @@
 from TempSensor import TempSensor
 from adminControlPanel import AdminControl
-from time import sleep
 from random import randint
 import asyncio
+import time
 
 
 class ZoneControl:
@@ -12,6 +12,7 @@ class ZoneControl:
 
     def __init__(self):
         self._temp_sensor = TempSensor()
+        self._admin_control = AdminControl()
         self._target_temp = 0
         self._total_seconds = 0
         self._state = 5
@@ -24,9 +25,11 @@ class ZoneControl:
         self._tminus_ambient_temp_increase = 20
         self._tminus_temp_decrease = 5  # Time takes until _zone_temp decreases by 1 Degree
         self._tminus_temp_increase = 5   # Time takes until _zone_temp increases by 1 Degree
+        self._start_time = 0
+        self._time_taken = 0
 
     '''
-    Returns the temperature value for the temp sensor class
+    Returns the temperature value for the temp sensor class 
     '''
 
     def get_temp(self):
@@ -40,7 +43,8 @@ class ZoneControl:
     '''
 
     def set_target_temp(self, input_target):
-        AdminControl.set_target_temp(AdminControl, input_target)
+        self._target_temp = self._admin_control.set_target_temp(input_target)
+        self._start_time = int(time.time())
 
     '''
     Gets the target temperature through the admin control panel
@@ -48,7 +52,7 @@ class ZoneControl:
     '''
 
     def get_target_temp(self):
-        self._target_temp = AdminControl.get_target_temp(AdminControl)
+        self._target_temp = self._admin_control.get_target_temp()
         return self._target_temp
 
     '''
@@ -56,14 +60,14 @@ class ZoneControl:
     '''
 
     def set_timer(self, input_time):
-        self._total_seconds = AdminControl.set_timer(AdminControl, input_time)
+        self._total_seconds = self._admin_control.set_timer(input_time)
 
     '''
     returns the timer value from the admin control panel
     '''
 
     def get_timer(self):
-        return AdminControl.get_timer(AdminControl)
+        return self._admin_control.get_timer()
 
     '''
     Toggles the fans
@@ -108,17 +112,64 @@ class ZoneControl:
                 self.reset_state()  # Turn heating off
             elif self._target_temp > self._zone_temp:
                 self._zone_temp += 1
-                await asyncio.sleep(self._tminus_temp_increase)  # 1 deg increase in temp takes _tminus_temp_increase seconds
+                await asyncio.sleep(self._tminus_temp_increase)  # 1C increase in temp takes _tminus_temp_increase sec
             elif self._target_temp < self._zone_temp:
                 self.toggle_fan()
-                await asyncio.sleep(self._tminus_temp_decrease)  # 1 deg decrease in temp takes _tminus_temp_decrease seconds
+                await asyncio.sleep(self._tminus_temp_decrease)  # 1C decrease in temp takes _tminus_temp_decrease sec
 
         elif self.get_state() == 1:  # If fan is on
             if self._target_temp == self._zone_temp:
                 self.reset_state()
             elif self._target_temp < self._zone_temp:
                 self._zone_temp -= 1
-                await asyncio.sleep(self._tminus_temp_decrease)  # 1 deg decrease in temp takes _tminus_temp_increase seconds
+                await asyncio.sleep(self._tminus_temp_decrease)  # 1C decrease in temp takes _tminus_temp_increase sec
             elif self._target_temp > self._zone_temp:
                 self.toggle_heater()
-                await asyncio.sleep(self._tminus_temp_decrease)  # 1 deg increase in temp takes _tminus_temp_decrease seconds
+                await asyncio.sleep(self._tminus_temp_decrease)  # 1C increase in temp takes _tminus_temp_decrease sec
+
+    def new_temperature_physics(self):
+        t1 = self._start_time
+        self._time_taken = int(time.time() - self._start_time)
+        t2 = self._time_taken
+        print(t1, "\n", t2)
+
+        if self._target_temp > self._zone_temp:
+            self.toggle_heater()
+        elif self._target_temp < self._zone_temp:
+            self.toggle_fan()
+
+        if self.get_state() == 5:  # If heating and fan are off
+            if self._zone_temp > self._temp:
+                if (t2 % self._tminus_ambient_temp_decrease == 0) and (t2 != 0):
+                    self._zone_temp -= (t2/self._tminus_ambient_temp_decrease)  # Checking if
+                    # _tminus_ambient_temp_decrease has elapsed, if so minus the multiple of that time that has elapsed
+                    self._start_time = time.time()  # reset start time
+            elif self._zone_temp < self._temp:
+                if (t2 % self._tminus_ambient_temp_increase == 0) and (t2 != 0):
+                    self._zone_temp += (t2 / self._tminus_ambient_temp_increase)  # Checking if
+                    # _tminus_ambient_temp_increase has elapsed, if so add the multiple of that time that has elapsed
+                    self._start_time = time.time()  # reset start time
+            elif self._zone_temp == self._temp:
+                self._zone_temp = self._temp
+
+        elif self.get_state() == 4:  # If heating is on
+            if self._target_temp == self._zone_temp:
+                self.reset_state()  # Turn heating off
+            elif self._target_temp > self._zone_temp:
+                if (t2 % self._tminus_temp_increase == 0) and (t2 != 0):
+                    self._zone_temp += (t2/self._tminus_temp_increase)  # Checking if _tminus_temp_increase seconds
+                    # has elapsed, if so add the multiple of that time that has
+                    self._start_time = time.time()  # reset start time
+            elif self._target_temp < self._zone_temp:
+                self.toggle_fan()
+
+        elif self.get_state() == 1:  # If fan is on
+            if self._target_temp == self._zone_temp:
+                self.reset_state()
+            elif self._target_temp < self._zone_temp:
+                if (t2 % self._tminus_temp_decrease == 0) and (t2 != 0):
+                    self._zone_temp -= (t2 / self._tminus_temp_decrease)  # Checking if _tminus_temp_decrease seconds
+                    # has elapsed, if so minus the multiple of that time that has elapsed
+                    self._start_time = time.time()  # reset start time
+            elif self._target_temp > self._zone_temp:
+                self.toggle_heater()
