dbb3db90c1467c8ba60b46b5e0d0251594ebac81
SeanMal1 <120354053@umail.ucc.ie>
2022-12-03 12:31:52 +0000

    Added small commented description to the top of test_adminInterface.py and test_interface.py
    Explained in comments: purpose of test suite, what was being tested and how it was being tested. Also updated instances of 'Interface' in class names to 'AdminInterface' and restructured the test cases to be at the top of the code and the methods underneath in test_adminInterface.py

--- test_interface.py
+++ test_interface.py
@@ -2,6 +2,62 @@ from adminControlPanel import AdminControl
 from zoneControlPanel import ZoneControl
 import unittest
 
+"""
+This test suite was developed for blackbox testing
+The standard user interface was dismantles and a seperate 'client' was created that invokes the same methods that are used in the interface.py file
+The methods needed to be altered to take in parameters as in the there original state the values were input into the methods through the use of .gets() and so on
+Furthermore for the methods to be suitable for testing, instead of displaying the result of the method execution to the gui it now returns the value
+This is needed for unit testing for using statements such as assertAlmostEqual as it needs a returned value for the method to test against the expected value
+The reaons values are returned rather than exceptions raised in these methods is due to the fact that when the user interacts with the interface, they do not see exceptions (they only show up in the terminal)
+Hence in tests where a value is invalid a default value may be returned
+When creating these tests the partitions were outlined first to help get a sense of what tests needed to be executed
+Then the inputs were organized into various ranges of inputs into eq classes and one value was picked per class
+In the case where a test case failed the method was then updated in order for the test to pass
+This led to a good bit of test driven style development.
+"""
+
+#PARTITION 1 if the target temperature entered by the user is above 30 or below 0 it is not valid
+class TestsetTempInterfaceInvalid(unittest.TestCase):
+     def test_set_temp_clicked(self):
+        self.assertAlmostEqual(set_temp_clicked(500), 30)
+        self.assertAlmostEqual(set_temp_clicked(-20), 0)
+
+#PARTITION 2 if the target temperature entered by the user is between 0 and 30 the input is valid
+class TestsetTempInterfaceValid(unittest.TestCase):
+    def test_set_temp_clicked(self):
+        self.assertAlmostEqual(set_temp_clicked(30),30)
+        self.assertAlmostEqual(set_temp_clicked(0),0)
+
+#PARTITION 3 if the user toggles the heating on the state is X and cooling is off
+class TestToggleHeatInterface(unittest.TestCase):
+    def test_toggle_heat_clicked(self):
+        self.assertAlmostEqual(toggle_heat_click(4),4)
+        self.assertAlmostEqual(toggle_heat_click(3),5)
+
+#PARTITION 4 if the user toggles the cooling on the state is y and heating is off
+class TestToggleCoolInterface(unittest.TestCase):
+    def test_toggle_cool_clicked(self):
+        self.assertAlmostEqual(toggle_cool_click(1),1)
+        self.assertAlmostEqual(toggle_cool_click(2),5)
+
+#PARTITION 5 if both heating and cooling are off the state is z (5)
+class TestToggleCoolandHeatInterface(unittest.TestCase):
+    def test_toggle_cool_heat_not_clicked(self):
+        self.assertAlmostEqual(z1._state, 5)
+        toggle_cool_click(1)
+        self.assertAlmostEqual(z1._state, 1)
+
+#PARTITION 6 if the user inputs a value below 0 when setting the time the input is not valid
+class TestsetTimeInterfaceInvalid(unittest.TestCase):
+    def test_set_time_clicked(self):
+        self.assertRaises(ValueError, set_time_clicked, -12)
+
+#PARTITION 7 if the user inputs a value that is equal to or greater than 0 the number is valid
+class TestsetTimeInterfaceValid(unittest.TestCase):
+    def test_set_time_clicked(self):
+        self.assertAlmostEqual(set_time_clicked(15), 15)
+
+
 adminPanel = AdminControl()
 z1 = ZoneControl()
 zonePanels = []
@@ -74,48 +130,4 @@ def set_time_clicked(t):
             result = time
         time = time - 1 """
     return result
-
-
-#PARTITION 1 if the target temperature entered by the user is above 30 or below 0 it is not valid
-class TestsetTempInterfaceInvalid(unittest.TestCase):
-     def test_set_temp_clicked(self):
-        self.assertAlmostEqual(set_temp_clicked(500), 30)
-        self.assertAlmostEqual(set_temp_clicked(-20), 0)
-
-#PARTITION 2 if the target temperature entered by the user is between 0 and 30 the input is valid
-class TestsetTempInterfaceValid(unittest.TestCase):
-    def test_set_temp_clicked(self):
-        self.assertAlmostEqual(set_temp_clicked(30),30)
-        self.assertAlmostEqual(set_temp_clicked(0),0)
-
-#PARTITION 3 if the user toggles the heating on the state is X and cooling is off
-class TestToggleHeatInterface(unittest.TestCase):
-    def test_toggle_heat_clicked(self):
-        self.assertAlmostEqual(toggle_heat_click(4),4)
-        self.assertAlmostEqual(toggle_heat_click(3),5)
-
-#PARTITION 4 if the user toggles the cooling on the state is y and heating is off
-class TestToggleCoolInterface(unittest.TestCase):
-    def test_toggle_cool_clicked(self):
-        self.assertAlmostEqual(toggle_cool_click(1),1)
-        self.assertAlmostEqual(toggle_cool_click(2),5)
-
-#PARTITION 5 if both heating and cooling are off the state is z (5)
-class TestToggleCoolandHeatInterface(unittest.TestCase):
-    def test_toggle_cool_heat_not_clicked(self):
-        self.assertAlmostEqual(z1._state, 5)
-        toggle_cool_click(1)
-        self.assertAlmostEqual(z1._state, 1)
-
-#PARTITION 6 if the user inputs a value below 0 when setting the time the input is not valid
-class TestsetTimeInterfaceInvalid(unittest.TestCase):
-    def test_set_time_clicked(self):
-        self.assertRaises(ValueError, set_time_clicked, -12)
-
-#PARTITION 7 if the user inputs a value that is equal to or greater than 0 the number is valid
-class TestsetTimeInterfaceValid(unittest.TestCase):
-    def test_set_time_clicked(self):
-        self.assertAlmostEqual(set_time_clicked(15), 15)
-
-
 
