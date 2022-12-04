3a487c7c9641fd83c4ae01d56ed3d48a9e062a73
SeanMal1 <120354053@umail.ucc.ie>
2022-12-04 11:45:15 +0000

    Update interface.py
    Added comments to the uncommented methods as in the adminInterface.py

--- interface.py
+++ interface.py
@@ -193,6 +193,11 @@ def tutorial_clicked():
         tutorialText.config(text=result5)
         tutorial_on = True
 
+
+'''
+Displays statistics regarding amount of power used, money spent and how much money saved based on the current state of the system
+This is done by taking the heating and cooling into account and for how long they were on/off
+'''
 def statistics_clicked():
     global statistics_on
     if statistics_on==True:
@@ -213,13 +218,22 @@ def statistics_clicked():
         statistics_on=True
         
 
-
+'''
+Returns the power usage amount for the statistics
+'''
 def get_power_usage():
     return z1.get_power_consumed()
 
+'''
+Returns the money spent amount for the statistics
+This is done by getting the power usage amount and multiplying it by the cost per hour variable
+'''
 def get_money_spent():
     return get_power_usage() * z1.get_cost_per_Kwh()
 
+'''
+Returns the money saved amount for the statistics
+'''
 def get_money_saved():
     return 50
 
